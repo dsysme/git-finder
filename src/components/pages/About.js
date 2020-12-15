b92e25401f910427d5971a95c2437d1d50d8f2f7
Sharon Shmorak <sharon.shmorak@gmail.com>
2020-12-15 14:03:18 +0200

    fixed version

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ function About() {
     <Fragment>
       <h1>About Github finder</h1>
       <p>A tool for searching github users.</p>
-      <p>V1.0.0</p>
+      <p>V1.0.8</p>
     </Fragment>
   );
 }
